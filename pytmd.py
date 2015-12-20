effb101662525a2f46c1713a3a666204c3d075cb
Andrew Dalgleish <git@ajd.net.au>
2015-12-20 12:01:44 +1100

    Simplified TMD parsing, removed hard-coded offsets.

--- pytmd.py
+++ pytmd.py
@@ -1,5 +1,40 @@
 
 import struct, functools, ctypes
+import binascii
+import hashlib
+
+def	get_signature_type(key, value):
+	# Taken from http://www.3dbrew.org/wiki/Title_metadata
+	signature_types = (
+		{ 'name':	b'RSA_4096_SHA1',	'value': 0x00010000,	'size': 512,	'padding': 0x3C, },
+		{ 'name':	b'RSA_2048_SHA1',	'value': 0x00010001,	'size': 256,	'padding': 0x3C, },
+		{ 'name':	b'ECDSA_SHA1',		'value': 0x00010002,	'size': 60,	'padding': 0x40, },
+		{ 'name':	b'RSA_4096_SHA256',	'value': 0x00010003,	'size': 512,	'padding': 0x3C, },
+		{ 'name':	b'RSA_2048_SHA256',	'value': 0x00010004,	'size': 256,	'padding': 0x3C, },
+		{ 'name':	b'ECDSA_SHA256',	'value': 0x00010005,	'size': 60,	'padding': 0x40, },
+	)
+	for t in signature_types:
+		if t[key] == value:
+			return t
+	assert False, "Unrecognised signature type"
+
+class unpacker(object):
+	def __init__(self, buffer):
+		self._buffer = buffer
+		self._offset = 0
+
+	def __call__(self, fmt):
+		result = struct.unpack_from(fmt, self._buffer, self._offset)
+		self._offset += struct.calcsize(fmt)
+		return result
+
+	def	seek(self, offset):
+		if (offset < len(self._buffer)):
+			self._offset = offset
+		return self._offset
+
+	def	tell(self):
+		return self._offset
 
 class TMD_CONTENT():
 	def __init__(self):
@@ -9,10 +44,15 @@ class TMD_CONTENT():
 		self.size = 0
 		self.sha1_hash = []
 	
-class TMD_PARSER(TMD_CONTENT):
+class TMD_PARSER():
 	""" Parses Wii U TMD """
 	def __init__(self, filepath=None):
-		self.file = open(filepath, 'rb')
+		#self.file = open(filepath, 'rb')
+
+		# Read in the entire file, then set up our unpacker
+		self.data = open(filepath, 'rb').read()
+		self.unpacker = unpacker(self.data)
+
 		self.tmd_signature_type = 0
 		self.tmd_signature = []
 		self.tmd_padding1 = []
@@ -24,56 +64,91 @@ class TMD_PARSER(TMD_CONTENT):
 		self.tmd_system_version = 0
 		self.tmd_title_id = 0
 		self.tmd_title_type = 0
-		self.tmd_group_ip = 0
-		self.tmd_reserved = []
+		self.tmd_group_id = 0
+		self.tmd_public_save_size = 0
+		self.tmd_private_save_size = 0
+		self.tmd_reserved1 = []
+		self.tmd_srl_flag = 0
+		self.tmd_reserved2 = []
 		self.tmd_access_rights = 0
 		self.tmd_title_version = 0
 		self.tmd_number_of_contents = 0
 		self.tmd_boot_index = 0
 		self.tmd_padding3 = 0
 		self.tmd_hash_table_hash = []
-		self.tmd_padding4 = 0
-		self.tmd_number_of_contents2 = 0
-		self.tmd_content_records_hash = []
-		self.tmd_padding4 = []
+		self.tmd_content_info_records = []
 		self.tmd_contents = []
 		
 	def readTmdHdr(self):
-		self.tmd_signature_type = struct.unpack('>I',self.file.read(4))[0]	#Signature Type
-		self.tmd_signature = struct.unpack('>256p',self.file.read(256))
-		self.tmd_padding1 = self.file.read(60)
-		self.tmd_issuer = 	self.file.read(64)
-		self.tmd_version = self.file.read(1)
-		self.tmd_ca_crl_version = self.file.read(1)
-		self.tmd_signer_crl_version = self.file.read(1)
-		self.tmd_padding2 = self.file.read(1)
-		self.tmd_system_version = '%016x' % struct.unpack('>Q',self.file.read(8))[0]
-		self.tmd_title_id =  '%016x' % struct.unpack('>Q',self.file.read(8))[0]
-		self.tmd_title_type = struct.unpack('>I',self.file.read(4))[0]
-		self.tmd_group_ip = self.file.read(2)
-		self.tmd_reserved = self.file.read(62)
-		self.tmd_access_rights = '%08x' % struct.unpack('>I',self.file.read(4))[0]
-		self.tmd_title_version = '%04x' % struct.unpack('>H', self.file.read(2))[0]
-		self.tmd_number_of_contents = struct.unpack('>H',self.file.read(2))[0]
-		self.tmd_boot_index = self.file.read(2)
-		self.tmd_padding3 = self.file.read(2)
-		self.tmd_hash_table_hash = self.file.read(32)
-		self.tmd_padding4 = self.file.read(2)
-		self.tmd_number_of_contents2 = self.file.read(2)
-		self.tmd_content_records_hash = self.file.read(32)
-		self.tmd_contents = []
+		self.tmd_signature_type		= self.unpacker('>I')[0]
+		sig_type = get_signature_type('value', self.tmd_signature_type)
+		#print(sig_type)
+		self.tmd_signature		= self.unpacker('>%ds' % sig_type['size'])[0]
+		self.tmd_padding1		= self.unpacker('>%ds' % sig_type['padding'])[0]
+		#print("o 0x%X" % self.unpacker._offset)
+		self.tmd_issuer			= self.unpacker('>64s')[0]
+		self.tmd_version		= self.unpacker('1c')[0]
+		self.tmd_ca_crl_version		= self.unpacker('1c')[0]
+		self.tmd_signer_crl_version	= self.unpacker('1c')[0]
+		self.tmd_padding2		= self.unpacker('1c')[0]
+		self.tmd_system_version		= '%016x' % self.unpacker('>Q')[0]
+		self.tmd_title_id		= '%016x' % self.unpacker('>Q')[0]
+		self.tmd_title_type		= self.unpacker('>I')[0]
+		self.tmd_group_id		= self.unpacker('>H')[0]
+		self.tmd_public_save_size	= self.unpacker('>I')[0]
+		self.tmd_private_save_size	= self.unpacker('>I')[0]
+		self.tmd_reserved1		= self.unpacker('>I')[0]
+		self.tmd_srl_flag		= self.unpacker('1c')[0]
+		self.tmd_reserved2		= self.unpacker('49s')[0]
+		self.tmd_access_rights		= '%08x' % self.unpacker('>I')[0]
+		self.tmd_title_version		= '%04x' % self.unpacker('>H')[0]
+		self.tmd_number_of_contents	= self.unpacker('>H')[0]
+		self.tmd_boot_index		= self.unpacker('>H')[0]
+		self.tmd_padding3		= self.unpacker('2s')[0]
+		self.tmd_hash_table_hash	= self.unpacker('>32s')[0]
+
+		# Validate the CIR table hash
+		offset = self.unpacker.tell()
+		cir_table_hash = hashlib.sha256(self.data[offset : offset + 0x24 * 64]).digest()
+		if (self.tmd_hash_table_hash != cir_table_hash):
+			print("Hash mismatch for CIR table")
+			print("Expected: %s" % self.tmd_hash_table_hash)
+			print("Found: %s" % cir_table_hash)
+		else:
+			print("Hash ok for CIR table")
+
+		# Read in all the content info records
+		self.tmd_content_info_records = []
+		for i in range(64):
+			(cir_offset, cir_count, cir_hash)	= self.unpacker('>HH32s')
+			self.tmd_content_info_records.append((cir_offset, cir_count, cir_hash))
+
+		# Validate the CIR entry hashes
+		# (Guessed starting offset, only ever seen the 1st CIR used to hash all the CCR)
+		offset = self.unpacker.tell()
+		for i in range(64):
+			(cir_offset, cir_count, cir_hash) = self.tmd_content_info_records[i]
+			if cir_count:
+				start = offset + cir_offset * 0x30
+				ccr_hash = hashlib.sha256(self.data[start : start + cir_count * 0x30]).digest()
+				if ccr_hash != cir_hash:
+					print("Hash mismatch for CIR entry %d" %  i)
+					print("Expected: %s" % cir_hash)
+					print("Found: %s" % ccr_hash)
+				else:
+					print("Hash ok for CIR entry %d" %  i)
 			
 	def ReadContent(self):
 		try:
 			self.readTmdHdr()			#Read in the TMD Header
-			self.file.seek(0xB04,0)		#HARD OFFSET to Contents, May break stuff.
+			self.tmd_contents = []
 			for content in range(0, self.tmd_number_of_contents):
 				tmd_cnt = TMD_CONTENT()
-				tmd_cnt.id = '%08x' % struct.unpack('>I',self.file.read(4))[0]
-				tmd_cnt.index = struct.unpack('>H',self.file.read(2))[0]
-				tmd_cnt.type = struct.unpack('>H',self.file.read(2))[0]
-				tmd_cnt.size = struct.unpack('>Q',self.file.read(8))[0]
-				tmd_cnt.sha1_hash = struct.unpack('>32s',self.file.read(32))[0]
+				tmd_cnt.id		= '%08x' % self.unpacker('>I')[0]
+				tmd_cnt.index		= self.unpacker('>H')[0]
+				tmd_cnt.type		= self.unpacker('>H')[0]
+				tmd_cnt.size		= self.unpacker('>Q')[0]
+				tmd_cnt.sha1_hash	= self.unpacker('>32s')[0]
 				self.tmd_contents.append(tmd_cnt)
 		except Exception as e:
 			print(e)
